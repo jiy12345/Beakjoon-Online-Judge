e521cda46f0f7986cf8c56a30907deadf2193aaa
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2021-09-18 15:18:26 +0900

    Update 9461 - 파도반 수열.cpp

--- Mathematics/9461 - 파도반 수열.cpp
+++ Mathematics/9461 - 파도반 수열.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 void solution(int T) {
+	// 규칙성을 보이지 않는 7항까지는 미리 저장하기
 	vector<long long> P = { 1, 1, 1, 2, 2, 3, 4 };
 
+	// 8항부터 100항까지는 계산하여 구하기
 	for (int i = 7; i < 100; i++) {
 		P.push_back(P[i - 1] + P[i - 5]);
 		P[i];
@@ -12,6 +14,7 @@ void solution(int T) {
 
 	int N;
 
+	// 요청받은 위치의 항 출력
 	for (int i = 0; i < T; i++) {
 		cin >> N;
 		cout << P[N - 1] << endl;
