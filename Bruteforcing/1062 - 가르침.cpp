1209320dbe91ab815135bf07f592acacfe5c4571
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2021-09-27 19:11:35 +0900

    Create 1062 - 가르침.cpp

---
+++ Bruteforcing/1062 - 가르침.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int N;
+int K;
+vector<int> word_bits;
+
+int solution(int cur_bit, int cur_index, int cur_alpha) {
+
+  int i, j;
+
+	if (K < 5) {
+		return 0;
+	}
+
+	// 기저사례: 알파벳 K개를 선택했을 경우
+	if (cur_alpha == K) {
+		// cout << cur_bit << endl;
+		int readable = 0;
+		for (i = 0;i < word_bits.size();i++) {
+			if (((cur_bit & word_bits[i])) == word_bits[i]) readable++;
+		}
+		return readable;
+	}
+
+
+	int max = 0;
+	for (int i = cur_index;i < 26;i++) {
+		if ((cur_bit & ((1 << i))) == (1 << i)) continue; // 이미 마킹된 알파벳일 경우 다음으로 진행하지 않음
+		int temp = solution((cur_bit | (1<<i)), i + 1, cur_alpha + 1);
+
+		if (temp > max) {
+			max = temp;
+		}
+	}
+	return max;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string temp;
+	cin >> N >> K;
+
+	for (int i = 0;i < N;i++) {
+		cin >> temp;
+		temp = temp.substr(4, temp.size() - 8);
+		int new_bit = 0;
+		// 각 단어별 비트 마킹
+		for (char c : temp) {
+			// or 연산이니까 둘중 1인 것은 1
+			// => 문자가 이미 마킹 되어 있을 경우 1인 상태가 유지되며, 그렇지 않을경우 1로 바뀜!
+			new_bit |= (1 << (c - 97));
+		}
+		// 마킹한 비트를 words 벡터에 넣어주기
+		word_bits.push_back(new_bit);
+	}
+
+	int cur_bit = 0; 
+	cur_bit |= (1 << ('a' - 'a'));
+	cur_bit |= (1 << ('c' - 'a'));
+	cur_bit |= (1 << ('i' - 'a'));
+	cur_bit |= (1 << ('n' - 'a'));
+	cur_bit |= (1 << ('t' - 'a'));
+
+
+	cout << solution(cur_bit, 0, 5);
+
+	return 0;
+}
