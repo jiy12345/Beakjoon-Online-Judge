2c0ecdf960e25c59b98e33058e98fe1f131c1097
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2021-11-09 03:47:19 +0900

    Update 11053 - 가장 긴 증가하는 부분 수열.cpp

--- Dynamic Programming/11053 - 가장 긴 증가하는 부분 수열.cpp
+++ Dynamic Programming/11053 - 가장 긴 증가하는 부분 수열.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 using namespace std;
 
-
-int max_lengths[1000] = { 0, };
+int dp[1000] = { 0, };
 // 값을 입력받을 배열
 int A[1000] = { 0, };
 
@@ -10,16 +9,18 @@ int N; // 수열의 길이
 
 int solution() {
     int answer = 0;
-    max_lengths[0] = 1;
+    dp[0] = 1;
     for (int i = 0;i < N;i++) {
-        int max_num = 0;
+        // 현재 항은 반드시 포함
+        dp[i] = 1;
+        // 이전의 값들 모두 고려
         for (int j = i - 1;j >= 0;j--) {
-            if (A[j] < A[i] && max_lengths[j] > max_num) {
-                max_num = max_lengths[j];
+            if (A[j] < A[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
-        max_lengths[i] = max_num + 1;
-        answer = max(max_lengths[i], answer);
+        // 현재 저장된 최장값과 비교하여 더 큰 값이면 갱신
+        answer = max(dp[i], answer);
     }
     return answer;
 }
