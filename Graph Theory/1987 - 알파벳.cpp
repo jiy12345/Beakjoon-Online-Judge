6439f0a6c589fdd3738272c877076ab335225c8d
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2021-11-27 04:07:54 +0900

    Add files via upload

---
+++ Graph Theory/1987 - 알파벳.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<string>
+#include <bitset>
+using namespace std;
+
+/*
+1. 문제 분석
+
+- 변수
+R: 보드의 세로길이
+C: 보드의 가로 길이
+
+- 변수 제한 사항
+R, C: 1~20
+
+- 문제 상황
+ 같은 알파벳이 적힌 칸을 두번 지날 수 없다고 할 때, 죄측 상단에서 시작해서 말이 최대로 갈 수 있는 칸이 몇 칸인지를 구하는 프로그램을 작성하라!
+
+ 첫 위치 또한 경로에 포함된다고 본다!
+
+2. 풀이 계획
+ DFS를 진행하되, 비트마스킹을 통해 어떤 알파벳이 있는지 계산을 계속해서 진행해야 할듯 하다!
+
+*/
+
+
+int dx[4] = { 1, -1, 0,  0 };
+int dy[4] = { 0,  0, 1, -1 };
+
+string board[20];
+
+int R, C;
+
+bool is_visited[20][20];
+
+int solution(int cur_i, int cur_j, int bit_masking, int cur_depth) {
+	int max_num = cur_depth;
+
+	// A: 65, Z: 90
+	
+	// 방문 표시 후 다음 탐색 시작
+	is_visited[cur_i][cur_j] = true;
+	for (int i = 0;i < 4;i++) {
+		int next_i = cur_i + dx[i];
+		int next_j = cur_j + dy[i];
+
+		// 범위를 벗어났을 경우에는 탐색을 진행하지 않음
+		if (next_i >= R || next_i < 0 || next_j >= C || next_j < 0) {
+			continue;
+		}
+		else {
+			char next_alphabet = board[next_i][next_j];
+			int next_alpha_bit = (1 << (next_alphabet - 65));
+			int bit_masking_result = bit_masking | next_alpha_bit;
+			// 지금까지 방문하지 않았던 알파벳일 경우에만 진행
+			if ((bit_masking_result != bit_masking) && is_visited[next_i][next_j] == false) {
+				max_num = max(max_num, solution(next_i, next_j, bit_masking_result, cur_depth + 1));
+			}
+		}
+	}
+	// 방문 표시 지우기
+	is_visited[cur_i][cur_j] = false;
+
+
+	return max_num;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	cin >> R >> C;
+
+	for (int i = 0;i < R;i++) {
+		cin >> board[i];
+	}
+
+	// 첫 알파벳에 대한 비트마스킹 진행
+	char start_alphabet = board[0][0];
+	int start_alpha_bit = (1 << (start_alphabet - 65));
+
+	cout << solution(0, 0, start_alpha_bit, 1);
+
+	return 0;
+}
