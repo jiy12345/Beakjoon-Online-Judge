08dcca99747df64798dc18ca513071893a3bfe68
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2021-12-29 05:18:05 +0900

    Update 1092 - 배.cpp

--- Greedy Algorithm/1092 - 배.cpp
+++ Greedy Algorithm/1092 - 배.cpp
@@ -3,44 +3,6 @@
 #include<algorithm>
 using namespace std;
 
-/*
-1. 문제 분석
-
-- 변수
-N: 크레인의 수
-M: 박스의 수
-
-- 변수 제한 사항
-N: 1~50
-M: 1~10,000
-
-qkrtmx		
-
- - 문제 상황
- 추의 무개들이 주어졌을 때, 이 추들을 사용하여 측정할 수 없는 양의 정수 무게 중 최소값을 구하여라
-
-2. 풀이 계획
-이걸 어떻게 정렬해야 할까?
-
-중복해서 뽑을 수 없고 순서가 상관 없으므로 조합 문제이긴 한데, 그렇다고 모든 조합을 다 구하기에는 2^N으로 지수적 시간복잡도를 보이므로 그렇게 구할 수는 없다.
-
-그렇다면 어떻게?
-
-DP, 조합에서 헤매고 있었는데, 정렬한 후 그리디로 간단히 표현이 가능한 것이었다!
-
-정렬한 상태에서 누적합보다 크다면,
-
-참고하여 정리하기!
-https://plplim.tistory.com/59
-https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=hongjg3229&logNo=221627349685
-
-Sn:
-
-3. 계획 검증
-
-
-*/
-
 int N, M;
 vector<int> weights;
 vector<int> cranes;
