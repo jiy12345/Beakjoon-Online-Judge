e66603c6a1a68511e738172be9fcc42e0bd04f39
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-02-15 17:55:00 +0900

    Update 1912 - 연속합.cpp

--- Dynamic Programming/1912 - 연속합.cpp
+++ Dynamic Programming/1912 - 연속합.cpp
@@ -5,7 +5,7 @@ int n;
 
 // 수열이 저장될 배열
 int sequence[100000];
-// 각 항까지의 연속합의 최대값이 저장될 배열
+// 각 항을 포함한 연속합의 최대값이 저장될 배열
 int dp[100000]; 
 
 int solution() {
@@ -16,17 +16,7 @@ int solution() {
 
 	// 점화식
 	for (int i = 1; i < n; i++) {
-		// 이전항까지의 연속합의 최대값이 음수일 경우
-		if (dp[i - 1] < 0) {
-			// 현 위치의 항이 가지고 잇는 값만 더함
-			dp[i] = sequence[i];
-		}
-		// 이전항까지의 연속합의 최대값이 양수일 경우
-		else {
-			// 현 위치의 항이 가지고 있는 값과 이전항까지의 연속합의 최대값을 더함 
-			dp[i] = sequence[i] + dp[i - 1];
-		}
-		// 현재 저장된 연속합의 최대값과 비교하여 더 큰값으로 저장
+        	dp[i] = max(dp[i - 1] + sequence[i], sequence[i]);
 		answer = max(dp[i], answer);
 	}
 	return answer;
