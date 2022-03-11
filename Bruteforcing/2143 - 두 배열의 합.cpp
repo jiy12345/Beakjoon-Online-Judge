f78e6016ccc9d161eb75499cf9b1d7b1eb15ca97
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-03-11 15:27:09 +0900

    Update 2143 - 두 배열의 합.cpp

--- Bruteforcing/2143 - 두 배열의 합.cpp
+++ Bruteforcing/2143 - 두 배열의 합.cpp
@@ -2,8 +2,18 @@
 #include<unordered_map>
 #include<vector>
 #include<numeric>
-
 using namespace std;
+
+/*
+풀이 1: unordered_map을 활용한 풀이
+unordered_map을 활용하여 같은 합의 개수를 기록하여 풀기
+
+사용한 메모리: 41224KB
+걸린 시간: 288ms
+
+같은 합을 하나로 함축하여 표현하기 때문에 계산의 횟수 자체는 줄어들겠지만, 
+map에 삽입하는 연산의 시간복잡도가 vector의 끝에 삽입하는 연산의 시간복잡도보다 높으므로 시간 자체는 더 길어지는 듯 하다!
+*/
 
 int n, m, T;
 vector<int> A(1000, 0);
