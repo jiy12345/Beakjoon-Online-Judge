e9ed278494345c4e6f07544560bc17a9831b9169
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-03-23 18:06:26 +0900

    Update 14395 - 4연산.cpp

--- Graph Theory/14395 - 4연산.cpp
+++ Graph Theory/14395 - 4연산.cpp
@@ -16,7 +16,7 @@ string solution() {
 	queue<pair<int, pair<long long, string>>> bfs_queue;
 	bfs_queue.push({ 0, {s, ""} });
 
-	priority_queue<string, vector<string>, greater<string>> answers;
+	string answer = "-1";
 
 	while (!bfs_queue.empty()) {
 		int cur_depth = bfs_queue.front().first;
@@ -25,21 +25,19 @@ string solution() {
 		bfs_queue.pop();
 		isVisited.insert(cur_s);
 
-		if (cur_depth > answerDepth) break;
-
 		if (cur_s == t) {
-			answerDepth = cur_depth;
-			answers.push(usedOperators);
+			answer = usedOperators;
+			break;
 		}
 
-		long long next_s = cur_s + cur_s;
+		long long next_s = cur_s * cur_s;
+		if (next_s <= t && isVisited.find(next_s) == isVisited.end()) bfs_queue.push({ cur_depth + 1, { next_s, (usedOperators + '*')} });
+
+		next_s = cur_s + cur_s;
 		if (next_s <= t && isVisited.find(next_s) == isVisited.end()) bfs_queue.push({cur_depth + 1, {next_s, (usedOperators + '+')}});
 		
 		next_s = cur_s - cur_s;
 		if (next_s <= t && isVisited.find(next_s) == isVisited.end()) bfs_queue.push({ cur_depth + 1, { next_s, (usedOperators + '-')} });
-		
-		next_s = cur_s * cur_s;
-		if (next_s <= t && isVisited.find(next_s) == isVisited.end()) bfs_queue.push({ cur_depth + 1, { next_s, (usedOperators + '*')} });
 		
 		if (cur_s != 0) {
 			next_s = cur_s / cur_s;
@@ -48,10 +46,7 @@ string solution() {
 		}
 	}
 
-	// 못찾을 경우
-	if (answers.empty()) return "-1";
-
-	return answers.top();
+	return answer;
 }
 
 int main() {
