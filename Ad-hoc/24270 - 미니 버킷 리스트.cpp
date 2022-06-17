d31a9881bee979d02fe40f318cac5b732f871ec2
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-06-17 23:41:04 +0900

    Add files via upload

---
+++ Ad-hoc/24270 - 미니 버킷 리스트.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, K;
+
+long long factorial(int r) {
+    long long result = 1;
+    for (int i = 1; i <= N; i++) {
+        result %= 1000000007;
+        result = result * (r + i) % 1000000007;
+    }
+
+    return result;
+}
+
+long long solution(int sum) {
+    int r = K - sum;
+
+    return factorial(r);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N >> K;
+
+    int sum = 0;
+    for (int i = 0; i < N; ++i) {
+        int a;
+        cin >> a;
+        sum += a;
+    }
+
+    cout << solution(sum);
+}
