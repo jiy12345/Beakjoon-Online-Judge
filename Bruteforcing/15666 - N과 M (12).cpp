d1d91ba93594f4ac4e9de2052d7d4c961e8e795d
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-07-02 14:03:22 +0900

    Add files via upload

---
+++ Bruteforcing/15666 - N과 M (12).cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int M, N;
+int sequence[8];
+vector<int>subSequence;
+
+void solution(int index) {
+	int curSize = subSequence.size();
+	if (curSize == M) {
+		for (int i = 0; i < curSize; i++) {
+			cout << subSequence[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	int isUsed = 0;
+	for (int i = index; i < N; i++) {
+		if (isUsed == sequence[i]) continue;
+		isUsed = sequence[i];
+		subSequence.push_back(sequence[i]);
+		solution(i);
+		subSequence.pop_back();
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> sequence[i];
+	}
+
+	sort(sequence, sequence + N);
+
+	solution(0);
+}
