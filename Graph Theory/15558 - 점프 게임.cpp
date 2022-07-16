3163d67d9c3df4291ceb89031246252eee0b91ad
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-07-16 19:21:27 +0900

    Update 15558 - 점프 게임.cpp

--- Graph Theory/15558 - 점프 게임.cpp
+++ Graph Theory/15558 - 점프 게임.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define LEFT 0
 #define RIGHT 1
 
-int di[3]			= { 1, -1, 0 };
+int di[3]		= { 1, -1, 0 };
 int dLeftOrRight[3]	= { 0, 0, 1 };
 
 int N, k;
@@ -18,13 +18,13 @@ bool bfs() {
 	bfs_queue.push({ 0, 0, LEFT });
 	while (!bfs_queue.empty()) {
 		int cur_depth		= bfs_queue.front()[0];
-		int cur_i			= bfs_queue.front()[1];
-		int cur_leftOrRight = bfs_queue.front()[2];
+		int cur_i		= bfs_queue.front()[1];
+		int cur_leftOrRight 	= bfs_queue.front()[2];
 		bfs_queue.pop();
 
 		for (int i = 0; i < 3; i++) {
-			int next_depth			= cur_depth + 1;
-			int next_i				= cur_i + di[i];
+			int next_depth		= cur_depth + 1;
+			int next_i		= cur_i + di[i];
 			int next_leftOrRight	= (cur_leftOrRight + dLeftOrRight[i]) % 2;
 			
 			// 승리 조건
