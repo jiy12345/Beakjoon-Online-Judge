1957423e5bc438ec4b5addcd13564441ca38fb3a
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-08-02 16:23:37 +0900

    Add files via upload

---
+++ Mathematics/1064 - 평행사변형.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<iomanip>
+using namespace std;
+
+/*
+xa, ya, xb, yb, xc, yc: 주어지는 세 좌표
+
+*/
+
+double x_A, y_A, x_B, y_B, x_C, y_C;
+
+void input() {
+	cin >> x_A >> y_A >> x_B >> y_B >> x_C >> y_C;
+}
+
+double solution() {
+	double deltaX_AB = fabs(x_A - x_B);
+	double deltaY_AB = fabs(y_A - y_B);
+	double deltaX_AC = fabs(x_A - x_C);
+	double deltaY_AC = fabs(y_A - y_C);
+
+	if (deltaX_AC * deltaY_AB == deltaX_AB * deltaY_AC) return -1;
+
+	double lineAB = sqrt((x_A - x_B) * (x_A - x_B) + (y_A - y_B) * (y_A - y_B));
+	double lineAC = sqrt((x_A - x_C) * (x_A - x_C) + (y_A - y_C) * (y_A - y_C));
+	double lineBC = sqrt((x_B - x_C) * (x_B - x_C) + (y_B - y_C) * (y_B - y_C));
+
+	pair<double, double> minMax = minmax({ (lineAB * 2 + lineAC * 2), (lineAB * 2 + lineBC * 2), (lineBC * 2 + lineAC * 2) });
+
+	return minMax.second - minMax.first;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	input();
+	
+	cout << setprecision(15) << solution();
+
+	return 0;
+}
