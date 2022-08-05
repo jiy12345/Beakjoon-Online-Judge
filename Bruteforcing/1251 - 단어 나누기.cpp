aa78c2e73d087c0891a02beae5476e5eec3530d6
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-08-05 23:15:55 +0900

    Update 1251 - 단어 나누기.cpp

--- Bruteforcing/1251 - 단어 나누기.cpp
+++ Bruteforcing/1251 - 단어 나누기.cpp
@@ -4,14 +4,6 @@
 #include<algorithm>
 using namespace std;
 
-//
-// 조합
-// 
-// 단어의 길이가 n일 때, 1부터 N - 1 까지의 수 중 2개 조합하여 나눌 위치 2자리 뽑고
-// 
-// 나누어 합친 결과를 현재 저장되어 있는 내용과 비교
-//
-
 string input;
 
 string solution() {
