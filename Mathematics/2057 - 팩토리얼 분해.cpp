0f4117887af82489910cd91a2c90aa600c180ce3
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-08-26 15:25:43 +0900

    Add files via upload

---
+++ Mathematics/2057 - 팩토리얼 분해.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+long long N;
+vector<long long>factorials;
+
+bool binCoef(long long curSum, int curIndex, int maxIndex) {
+	if (curIndex == maxIndex) {
+		if (curSum == N) return true;
+		else			 return false;
+	}
+
+	for (int i = 0; i < 2; iS++) {
+		if (binCoef(curSum + factorials[curIndex] * i, curIndex + 1, maxIndex) == true)
+			return true;
+	}
+
+	return false;
+}
+
+string solution() {
+	if (N == 0) return "NO";
+	int maxIndex = 0;
+	long long curFactorial = 1;
+
+	while (true) {
+		if (curFactorial > N) break;
+		factorials.push_back(curFactorial);
+		curFactorial *= ++maxIndex;
+	}
+
+	return (binCoef(0, 0, maxIndex) ? "YES" : "NO");
+}
+
+int main() {
+	cin >> N;
+
+	cout << solution();
+}
