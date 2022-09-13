28edc42b3ce0aa239a6d8e91ee85861e73894789
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-09-13 16:32:10 +0900

    Add files via upload

---
+++ 2993 - 세 부분.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+string input;
+
+string solution() {
+	int strSize = input.size();
+	string answer = input;
+
+	
+	for (int i = 1; i < strSize; i++) {
+		for (int j = i + 1; j < strSize; j++) {
+			string subStrs[3];
+			subStrs[0] = input.substr(0, i);
+			subStrs[1] = input.substr(i, j - i);
+			subStrs[2] = input.substr(j);
+
+			for (int k = 0; k < 3; k++) {
+				reverse(subStrs[k].begin(), subStrs[k].end());
+			}
+
+			answer = min(answer, subStrs[0] + subStrs[1] + subStrs[2]);
+		}
+	}
+
+	return answer;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> input;
+	cout << solution();
+}
