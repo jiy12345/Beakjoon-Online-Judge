0627a4db752eeea30cae9a2a2f465363a4c8942e
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2022-12-02 10:14:23 +0900

    Add files via upload

---
+++ Data Structure/1158 - 요세푸스 문제.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int N, K;
+queue<int> q;
+
+void solution() {
+	for (int i = 1; i <= N; i++)
+		q.push(i);
+
+	cout << "<";
+	while (true) {
+
+		for (int i = 1; i < K; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+
+		if (q.size() == 1) break;
+		cout << ", ";
+		q.pop();
+	}
+	cout << ">";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> K;
+
+	solution();
+}
