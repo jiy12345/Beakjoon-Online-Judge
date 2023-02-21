4f037c58b4e4229ac3041aa2cf680a623b0a85dd
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-02-21 13:25:14 +0900

    Add files via upload

---
+++ Mathematics/9375 - 패션왕 신해빈.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+
+int T, n;
+
+
+int solution() {
+	unordered_map<string, unordered_set<string>> clothes_list;
+
+	for (int i = 0;i < n;i++) {
+		string clothes_name, sort;
+
+		cin >> clothes_name >> sort;
+
+		clothes_list[sort].insert(clothes_name);
+	}
+
+	int cases = 1;
+
+	for (auto cur_sort : clothes_list) {
+		cases *= (cur_sort.second.size() + 1);
+	}
+
+	return cases - 1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> T;
+
+	for (int i = 0;i < T;i++) {
+		cin >> n;
+
+		cout << solution() << '\n';
+	}
+}
